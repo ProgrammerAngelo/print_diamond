e46981162501815fd07eb0ae8b66a451c88be9b9
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-09-27 07:40:08 -0400

    added a method for getting the odd number

--- print_diamond.py
+++ print_diamond.py
@@ -1,2 +1,11 @@
 #write a code that prints a diamond shape * using odd number
+def odd_integer():
+    #for inputing odd number
+    number = int(input("Please enter an odd number: "))
+    #for checking if the number is odd
+    if number % 2 != 0:
+        return number #return the odd integer
+    else:
+        print("Invalid. Please enter an odd number ")
+        return odd_integer() #for valid input
 #method for looping that says"Please provide an odd integer"
